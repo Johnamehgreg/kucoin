2c80a84937c0adf5c321ac61428480c8f1c8483f
Johnamehgreg <88066580+Johnamehgreg@users.noreply.github.com>
2022-04-20 20:56:47 +0100

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var mobileBtn = document.getElementById("mobileSign");
 var emailBtn = document.getElementById("EmailSign");
 
+    var input = document.querySelector(".iti__flag-containe");
+
+
 var emailInput = document.getElementById("EmailSignInputI");
 var emailLabel = document.getElementById("EmailSignInputL");
 
@@ -17,13 +20,13 @@ const inputElement = document.getElementById('inputButton')
 
 
 const changeSign =(type) =>{
-  
+    // input.innerHTML = '.iti__flag-containe{display: none}'
     if(type == 'mobile'){
         mobileBtn.classList.add("btn-active");
         emailBtn.classList.remove("btn-active");
         
         emailInput.classList.add("disactivate-input")
-        emailLabel.classList.add("disactivate-input")
+        emailLabel.innerText ='Phone Number'
 
         mobileLabel.classList.remove("disactivate-input")
         mobileInput.classList.remove("disactivate-input")
@@ -35,7 +38,7 @@ const changeSign =(type) =>{
         emailBtn.classList.add("btn-active");
 
         emailInput.classList.remove("disactivate-input")
-        emailLabel.classList.remove("disactivate-input")
+        emailLabel.innerText ='Email/Sub-Account'
 
         mobileLabel.classList.add("disactivate-input")
         mobileInput.classList.add("disactivate-input")
